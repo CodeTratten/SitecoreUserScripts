0eb1eb22e88b6ae038d3bbe237aeaf9065ab4707
CodeTratten <anders@svart.nu>
2020-02-21 14:59:36 +0100

    Link to data source.

---
+++ link-to-data-source.sitecore.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         Link to data source
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @match        */sitecore/shell/applications/field%20editor.aspx?mo=mini&hdl=*
+// @grant        none
+// @require http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js
+// ==/UserScript==
+
+(function($) {
+    var value = $('.scContentButtons').parent().find('.scContentControl').val();
+    if(value.length === 0){
+        return;
+    }
+    $('.scContentButtons').append('<a href="/sitecore/shell/Applications/Content%20Editor.aspx?fo=' + escape(value) + '" target="_blank" class="scContentButton">Go to item</a>');
+})(jQuery);
+
